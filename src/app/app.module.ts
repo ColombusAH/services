eae31c1c25942284445f39bac13212898b751e4f
colombusAH <--add>
2022-12-01 18:10:31 +0200

    skaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsPageComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit {
+
+  cartCount: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
