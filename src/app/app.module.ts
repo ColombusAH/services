ac5c702be9f8aae70361f78d270c35ccc8af9442
colombusAH <--add>
2022-12-08 18:56:26 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
 import { LayoutComponent } from './layout/layout.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProductComponent } from './products-page/components/product/product.component';
+
+const routes: Routes = [{
+  path: 'products',
+  component: ProductsPageComponent,
+  children: [
+    {
+      path: ':id',
+      component: ProductComponent,
+    },
+  ]
+},
+{
+  path: 'profile',
+  component: ProfileComponent,
+},
+{
+  path: 'settings',
+  component: SettingsComponent,
+}];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsPageComponent,
-    LayoutComponent
+    LayoutComponent,
+    ProfileComponent,
+    SettingsComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
